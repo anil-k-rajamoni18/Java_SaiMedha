1929a0ed0aaea3e0d322badc0e048ea4ad2d5c85
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2022-12-10 12:41:21 +0530

    added files

---
+++ Java-Dec-10/Demo.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class A{
+		void m1(){ System.out.println("A CLASS METHOD");}
+}
+
+class B extends A{
+public static void main(String[] args){
+	
+	System.out.println("CLass B main");
+}
+}
+
+public class Demo{
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello");
+	}
+}
