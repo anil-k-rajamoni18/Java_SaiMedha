5a68eb19cd8595a83b4a8b2df30e5129c512bfee
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2022-12-10 19:06:09 +0530

    added code files

---
+++ Chapter-101-CodeExamples/src/main/java/com/classs101/Introduction.java
@@ -0,0 +1,16 @@
+package com.classs101;
+
+public class Introduction {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World..");
+
+
+        int a = 10;
+        int b= 20;
+        System.out.println(a=b);
+        System.out.println("a = " + a);
+        System.out.println(b);
+
+    }
+}
